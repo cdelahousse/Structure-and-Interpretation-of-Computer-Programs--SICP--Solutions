34e0b390323266f34ac49c3713f7ef129f66ee18
Christian Delahousse <christian.delahousse@gmail.com>
2011-10-01 17:56:38 -0400

    review

--- 1.3.scm
+++ 1.3.scm
@@ -6,3 +6,16 @@
 (define (square x) (* x x))
 (define (sum-of-squares x y) 
   (+ (square x) (square y)))
+
+;I just wanted to modify the idea above to work with unlimited arguments
+(define (sum-of-list-squares . args)
+  (define (square-iter l)
+    (cond ((null? (cdr l)) (square (car l)))
+          (else (+ (square (car l))
+                   (square-iter (cdr l))))))
+  (square-iter args))
+
+;Test
+(if (eq? (sum-of-list-squares 1 2 3 4 5) 55)
+    (display 'YEAH)
+    (display 'NAW))
